18e12641704a88fd47e5a12ef125d8f52df477c9
Jimmy Jeong <piremi@gmail.com>
2020-06-23 19:25:46 -0700

    Create GameLoop structure
    Blackjack.java
    playerDeck
    dealerDeck

--- src/com/codewithjimmy/Blackjack.java
+++ src/com/codewithjimmy/Blackjack.java
@@ -18,5 +18,32 @@ public class Blackjack {
 
         Scanner userInput = new Scanner(System.in);
 
+        //Game Loop
+        while(playerMoney > 0){
+            //Play on!
+            //Take the Players bet
+            System.out.println("You have $" + playerMoney + ", how much would you like to bet?");
+            double playerBet = userInput.nextDouble();
+            if(playerBet > playerMoney){
+                System.out.println("You cannot bet more than you have. Please leave.");
+                break;
+            }
+
+            //Start Dealing //might change playingCards to 'playingDeck' after merging to master.
+
+            //Player gets two cards
+            playerDeck.draw(playingCards);
+            playerDeck.draw(playingCards);
+
+            //dealer gets two cards
+            dealerDeck.draw(playingCards);
+            dealerDeck.draw(playingCards);
+
+            while(true){
+                System.out.println("Your hand:");
+                System.out.println(playerDeck.toString());
+            }
+        }
+        System.out.println("Game Over! You are out of money. @.@");
     }
 }
